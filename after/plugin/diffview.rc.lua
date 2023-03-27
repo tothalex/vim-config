769cad0c50f13a9ba09ed3a5f69ffd32910cc8a9
Toth Alex <tothalex097@gmail.com>
2023-03-27 12:20:16 +0200

    feat: add file git history keymap

--- after/plugin/diffview.rc.lua
+++ after/plugin/diffview.rc.lua
@@ -3,3 +3,4 @@ local keymap = vim.keymap
 keymap.set('n', ';d', ':DiffviewOpen<CR>', { silent = true })
 keymap.set('n', ';q', ':DiffviewClose<CR>', { silent = true })
 keymap.set('n', ';h', ':DiffviewFileHistory<CR>', { silent = true })
+keymap.set('n', ';t', ':DiffviewFileHistory %<CR>', { silent = true })
